88a8d8400696ad171eb05fb613775f6b407e5797
Neel Goswami <neelgoswami.cs@gmail.com>
2023-02-06 05:56:51 -0800

    Added key validation for getOriginalUrl endpoint

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Url;
 use Illuminate\Http\Request;
 use App\Services\UrlShortenerService;
+use Illuminate\Support\Facades\Validator;
 
 class UrlController extends Controller
 {
@@ -43,6 +44,15 @@ class UrlController extends Controller
 
     public function getOriginalUrl(Request $request)
     {
+        // key Validation
+        $validator = Validator::make(['key' => $request->key], [
+            'key' => 'regex:/^[0-9a-zA-z]{5}$/i'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['original_url' => '']);
+        }
+
         $originalUrl = (new Url)->getOriginalUrlByKey($request->key);
 
         return response()->json(['original_url' => $originalUrl]);
